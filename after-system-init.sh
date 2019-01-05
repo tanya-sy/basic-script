82c7169843d7c40109c7b695afd7fcaeb1e06d61
tanya-sy <846561165@qq.com>
2019-01-05 17:10:00 +0800

    add some item

--- after-system-init.sh
+++ after-system-init.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-##just for test env
 
 systemctl stop NetworkManager
 systemctl disable NetworkManager
 systemctl stop firewalld
 systemctl disable firewalld
-sed -i 's/=enforcing/=disabled/' /etc/selinux/config
 
+sed -i 's/=enforcing/=disabled/' /etc/selinux/config
 sed -i 's/^#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config
 sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
 chmod +x /etc/rc.d/rc.local
 
 
-yum install -y bash-completion rsync  vim wget ntpdate lrzsz  net-tools
+yum install -y bash-completion rsync  vim wget ntpdate lrzsz  net-tools htop lsof 
 
 ####ntpdate time
 echo "*/30 * * * * /usr/sbin/ntpdate time7.aliyun.com >/dev/null 2>&1" >> /var/spool/cron/root
 
+###增大文件描述符
+echo "* - nofile 65535" >>/etc/security/limits.conf
+echo "* - noproc 65535" >>/etc/security/limits.conf
 
 ##获得系统的基本信息
 ##memory
